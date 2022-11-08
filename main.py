f23c0f87a942d2aa5a8c78c85b91ab65c7b99bb7
Asher Stout <stoutashe@duck-island.ecs.vuw.ac.nz>
2022-11-08 16:37:20 +1300

    Added stub main.py, updated gitignore

---
+++ main.py
@@ -0,0 +1,15 @@
+#  Main script for Information analysis of the reverse process of Diffusion models
+#  Asher Stout
+
+
+def main():
+    """
+    Configures the script
+
+    :return:
+    """
+    return 0
+
+
+if __name__ == "main":
+    main()
