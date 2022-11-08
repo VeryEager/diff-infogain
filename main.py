e9e9ea1acce4f44c85991c663c151d02757162de
Asher Stout <stoutashe@duck-island.ecs.vuw.ac.nz>
2022-11-08 17:17:55 +1300

    Added arg parsing, logging, stub mode files

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 #  Main script for Information analysis of the reverse process of Diffusion models
 #  Asher Stout
 
+import tqdm
+import argparse
+import logging
+import PIL
+import seaborn
+import pandas as pd
+import numpy as np
+
 
 def main():
     """
-    Configures the script
+    Configures valid arguments
 
-    :return:
+    :return: initialized ArgumentParser
     """
-    return 0
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "--inp",
+        type=str,
+        default="samples/",
+        help="path to input directory; should contain single folder of reverse process images"
+    )
+    args.add_argument(
+        "--out",
+        nargs='?',
+        type=str,
+        default="output/",
+        help="path to output directory"
+    )
+    args.add_argument(
+        "--analysis",
+        type=str,
+        default="info",
+        help="type of analysis to perform on the data"
+    )
+    args.add_argument(
+        "--log",
+        nargs='?',
+        type=str,
+        default="log.txt",
+        help="file to print logs to"
+    )
+
+    return args.parse_args()
 
 
-if __name__ == "main":
-    main()
+if __name__ == "__main__":
+    arg = main()
+    logging.basicConfig(filename=arg.log, level=logging.DEBUG,
+                        format="%(asctime)s: %(message)s", filemode="w")
+
+    # Delegate to correct mode file
+    
